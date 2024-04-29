29fdc8a2020acd210b367dfef6a1e6e5ad1e232c
shalkichauhan <shalkichauhan@gmail.com>
2024-04-29 16:51:33 +1000

    C#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[,] myGroceryList = new string[,] { {"apple", "Banana" },{"orange","guvava"}};
+            Console.WriteLine(myGroceryList[1,0]);
+            Dictionary<string, string> myDict = new Dictionary<string, string>(){{"shalki","job"},{"Zubin","work"}};
+            Console.WriteLine(myDict["shalki"]);
+            string[] fruitList = { "grapes", "avacado" };
+            Console.WriteLine(fruitList[1]);
+
+            List<string> myVegList =  new List<string>(){"peas", "potato"};
+            myVegList.Add("eggplant");
+            Console.WriteLine(myVegList[2]);
+            
+            
+            Console.ReadLine();
+
+
+
+
+        }
+        
+        
+    }
+}
